ab24c455001c1d8c1842ed780c42136e0fbeea0f
Abhiram <abhiram.satha@gmail.com>
2021-09-19 22:24:33 +0000

    Commit remaining file

---
+++ lighthouse-js-fundamentals/beers.js
@@ -0,0 +1,12 @@
+var x = 99;
+
+while (x >=1) {
+    if (x >=2) {
+        console.log(x + " bottles of juice on the wall! "+ x + " bottles of juice! Take one down, pass it around... " + x -1 + " bottles of juice on the wall!");
+        x -=1;
+    } else {
+        console.log(x + " bottle of juice on the wall! "+ x + " bottle of juice! Take one down, pass it around... "+ x -1 + " bottles of juice on the wall!");
+        x -=1;
+    }
+ 
+}
